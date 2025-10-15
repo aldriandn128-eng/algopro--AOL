381ecc6fb37991ddc6216dd81e16b873713201c3
aldriandn128-eng <aldriandn128@gmail.com>
2025-10-15 23:26:25 +0700

    Update algopro.c

--- algopro.c
+++ algopro.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+int main (){
 
 #define max_booking 50  //ini maksimal orangnya, kalau lebih dari ini program tidak akan mulai atau muncul
 #define max_kamar 5
@@ -20,6 +21,14 @@ char daftarKamar[max_kamar][50] = {
 "Kamar 80 - Vip",
 "Kamar 90 - Family",
 };
+//perlu codingan fungsi untuk menampilkan daftar kamarnya
+
+//perlu codingan fungsi untuk menampilkan booking kamar
+
+//lanjutin ajaa lahhhh, bingung gw
+
+return 0;
+}
 
 
 
